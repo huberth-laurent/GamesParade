8ce95ecd6e14daa6c38724d671fe667414e2a353
Autoquark <github@autoquark.33mail.com>
2020-10-12 21:28:22 +0100

    WIP phone interface, move out of inner folder

---
+++ Assets/Behaviours/AppAreaBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Common;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Behaviours
+{
+    public class AppAreaBehaviour : MonoBehaviour
+    {
+        private List<GameObject> _children;
+
+        private void Start()
+        {
+            _children = transform.Children().Select(x => x.gameObject)
+                .ToList();
+        }
+
+        public void SetActiveApp(GameObject root)
+        {
+            foreach(var child in _children)
+            {
+                child.SetActive(false);
+            }
+
+            root.SetActive(true);
+        }
+    }
+}
