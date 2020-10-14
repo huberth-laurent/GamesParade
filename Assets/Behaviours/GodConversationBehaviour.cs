e85dd80028dde51f08a662fcc6977510fed61080
Autoquark <github@autoquark.33mail.com>
2020-10-14 17:20:19 +0100

    God conversation scrolls

--- Assets/Behaviours/GodConversationBehaviour.cs
+++ Assets/Behaviours/GodConversationBehaviour.cs
@@ -13,11 +13,17 @@ namespace Assets.Behaviours
     class GodConversationBehaviour : MonoBehaviour
     {
         private HashSet<Text> _texts = new HashSet<Text>();
+        private Lazy<GameObject> _listRoot;
+
+        public GodConversationBehaviour()
+        {
+            _listRoot = new Lazy<GameObject>(() => transform.Find("MessageList/Viewport/Content").gameObject);
+        }
 
         private void Start()
         {
             // Get rid of the example texts
-            foreach (var child in transform.Children())
+            foreach (var child in _listRoot.Value.transform.Children())
             {
                 Destroy(child.gameObject);
             }
@@ -33,7 +39,7 @@ namespace Assets.Behaviours
         {
             foreach (var text in Database.GetAllSentTexts().Where(x => !_texts.Contains(x)).OrderBy(x => x.SentAtTime))
             {
-                var ui = Instantiate(text.isOutgoing ? CommonPrefabs.Instance.SentText : CommonPrefabs.Instance.ReceivedText, transform);
+                var ui = Instantiate(text.isOutgoing ? CommonPrefabs.Instance.SentText : CommonPrefabs.Instance.ReceivedText, _listRoot.Value.transform);
                 ui.GetComponentInChildren<TMP_Text>().text = text.message;
                 _texts.Add(text);
             }
