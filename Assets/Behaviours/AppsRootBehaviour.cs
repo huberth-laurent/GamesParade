908ef4e55e19f1378ca15487501948f69565de65
Autoquark <github@autoquark.33mail.com>
2020-10-14 13:38:00 +0100

    Non functional settings screen

--- Assets/Behaviours/AppsRootBehaviour.cs
+++ Assets/Behaviours/AppsRootBehaviour.cs
@@ -11,6 +11,7 @@ namespace Assets.Behaviours
     class AppsRootBehaviour : OneChildActiveBehaviour
     {
         private Lazy<GameObject> _homeRoot;
+        private Lazy<GameObject> _settingsRoot;
         private Lazy<GameObject> _messagesRoot;
         private Lazy<MessagesInnerRootBehaviour> _messagesInnerRoot;
         private Lazy<GameObject> _twitterRoot;
@@ -19,6 +20,7 @@ namespace Assets.Behaviours
         public AppsRootBehaviour()
         {
             _homeRoot = new Lazy<GameObject>(() => transform.Find("Homescreen").gameObject);
+            _settingsRoot = new Lazy<GameObject>(() => transform.Find("SettingsApp").gameObject);
             _messagesRoot = new Lazy<GameObject>(() => transform.Find("MessagesApp").gameObject);
             _messagesInnerRoot = new Lazy<MessagesInnerRootBehaviour>(() => _messagesRoot.Value.transform.Find("InnerAppArea")
                 .GetComponent<MessagesInnerRootBehaviour>());
@@ -28,6 +30,8 @@ namespace Assets.Behaviours
         }
 
         public void DisplayHome() => SetActiveChild(_homeRoot.Value);
+
+        public void DisplaySettings() => SetActiveChild(_settingsRoot.Value);
 
         public void DisplayMessages()
         {
